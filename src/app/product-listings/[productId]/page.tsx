f48ddb7e3f714f17281d9137ece2ff5c0a8a19e2
Mutahir-15 <mutahir.bin.athar2005@gmail.com>
2025-01-22 09:15:09 +0500

    AVION

--- src/app/product-listings/[productId]/page.tsx
+++ src/app/product-listings/[productId]/page.tsx
@@ -1,56 +1,11 @@
-import { Product } from '@/lib/api';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react'
 
-interface ProductPageProps {
-  product?: Product; 
+function page() {
+  return (
+    <div className='max-w-[1440px] mx-auto text-center p-10 '>
+      <h1 className='text-3xl'><span className='font-bold'>Attention:</span> An Error Occured At the time of Deployment.<br/> Dynamic product details will be sorted SOON.<br/><span className='font-bold uppercase'>Inshallah!</span></h1>
+    </div>
+  )
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
-  if (!product?.dimensions) {
-    return <p>Product not found</p>;
-  }
-
-  const { width, height, depth } = product.dimensions;
-
-  return (
-    <main>
-      <section className="max-w-[1440px] grid grid-cols-1 lg:grid-cols-2 mt-2 lg:mt-10 gap-8 mx-auto justify-center items-center">
-        {/* Image Section */}
-        <div className="w-full lg:w-auto lg:max-w-lg">
-          <Image
-            src={product.image_url}
-            alt={product.name}
-            width={500}
-            height={600}
-            className="object-contain mb-5 lg:mb-10"
-          />
-        </div>
-        {/* Product Details Section */}
-        <div className="flex flex-col gap-4 mb-5 lg:mb-10 lg:px-5">
-          <h1 className="text-4xl font-bold">{product.name}</h1>
-          <p className="text-2xl font-semibold">${product.price}</p>
-          <p className="text-lg">{product.description}</p>
-          <p className="text-2xl font-semibold">Features:</p>
-          <p className="text-lg">{product.features || 'No features available'}</p>
-          <p className="text-2xl font-semibold">Stock:</p>
-          <p className="text-lg">{product.quantity}</p>
-          <p className="text-2xl font-semibold">Dimensions:</p>
-          <p className="text-lg">
-            {`Width: ${width} cm, Height: ${height} cm, Depth: ${depth} cm`}
-          </p>
-          <h2 className="text-2xl font-bold mt-4">Tags</h2>
-          <p className="text-lg">{product.tags?.join(', ') || 'No tags available'}</p>
-          {/* Button */}
-          <Link href="#">
-            <button className="mt-8 px-4 py-2 sm:px-6 sm:py-3 bg-customColors-dark-primary text-white font-bold hover:bg-customColors-border-dark hover:text-black transition-colors duration-300 rounded">
-              Add to Cart
-            </button>
-          </Link>
-        </div>
-      </section>
-    </main>
-  );
-};
-
-export default ProductPage;
+export default page
