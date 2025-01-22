167d7ce9f7c0312c139d33bde79d6daf1e2f8017
Mutahir-15 <mutahir.bin.athar2005@gmail.com>
2025-01-22 05:15:51 +0500

    AVION

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+// pages/cart.tsx
+
+import React from 'react';
+import ShoppingCart from '@/components/Cart';
+
+const CartPage: React.FC = () => {
+  return (
+    <div>
+      <ShoppingCart />
+    </div>
+  );
+};
+
+export default CartPage;
+
